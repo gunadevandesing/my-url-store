ee639d31994cecf3abb018b77c6645e4c4c063a3
gunadevandesing <gunadevandesing@gmail.com>
2024-04-22 04:08:53 +0000

    latest links added

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,267 +1,301 @@
-export const linksAvailable = [
+export const linksAvailable = [  {
+  category: "Prompt tuning studio",
+  links: [
   {
-    category: "Risk control platform",
-    links: [
-      {
-        id: "rcp-1",
-        name: "Scrum team 1 sprint board",
-        url: "https://dev.azure.com/TfO-DevOps-CCT-US/Risk%20Control%20Platform_244725/_sprints/backlog/Scrum-Team1/Risk%20Control%20Platform_244725/",
-      },
-      {
-        id: "rcp-2",
-        name: "React repos",
-        url: "https://dev.azure.com/TfO-DevOps-CCT-US/Risk%20Control%20Platform_244725/_git/ControlMonitoringFramework_UI/branches",
-      },
-      {
-        id: "rcp-3",
-        name: "API repos",
-        url: "https://dev.azure.com/TfO-DevOps-CCT-US/Risk%20Control%20Platform_244725/_git/CMF_API_LandscapeControl/branches",
-      },
-      {
-        id: "rcp-4",
-        name: "Pipelines",
-        url: "https://dev.azure.com/TfO-DevOps-CCT-US/Risk%20Control%20Platform_244725/_build",
-      },
-      {
-        id: "rcp-5",
-        name: "Invision Prototype",
-        url: "https://accentureoperations.invisionapp.com/console/share/MD7SG8T5V23/985480080/play",
-      },
-      {
-        id: "rcp-6",
-        name: "Sketch",
-        url: "https://www.sketch.com/s/7986ae17-5651-4304-aea1-95ac34f6136b/a/GowZO77",
-      },
-      {
-        id: "rcp-7",
-        name: "Data model excel",
-        url: "https://ts.accenture.com/:x:/s/RiskFrameworkPlatform/EUyI9hoJUWxCh1hSHdAVqdUBtKJlhAQmwvrwrtoBMqUmow?email=gunadevan.desing%40accenture.com&e=4%3AXGzyVg&at=9",
-      },
-      {
-        id: "rcp-8",
-        name: "Leave tracker",
-        url: "https://ts.accenture.com/:x:/r/sites/RiskFrameworkPlatform/_layouts/15/Doc.aspx?sourcedoc=%7BDED5F598-003D-4865-83BB-150630FC2077%7D&file=RCP-LeaveTracker-FY23.xlsx&action=default&mobileredirect=true&cid=538b9adf-cbc8-47d4-8e03-809cb3781e01",
-      },
-      {
-        id: "rcp-9",
-        name: "GCP console",
-        url: "https://console.cloud.google.com/welcome?project=tfo-usn-4959-dev-riskframework",
-      },
-    ],
+  id: "pts-1",
+      name: "DTP Assist",
+      url: "https://dev.azure.com/TfO-DevOps-CCT-US/SynOps_31124/_sprints/taskboard/DTP%20Assist/SynOps_31124/DTP%20Assist%20Dasboard/Sprint%202",
   },
   {
-    category: "Prompt tuning studio",
-    links: [
-      {
-        id: "pts-1",
-        name: "PTS React Repo",
-        url: "https://dev.azure.com/TfO-DevOps-CCT-US/SynOps_31124/_git/SynOps.MSTeamAppEnablementUI/branches",
-      },
-      {
-        id: "pts-2",
-        name: "PTS Pipelines",
-        url: "https://dev.azure.com/TfO-DevOps-CCT-US/SynOps_31124/_build",
-      },
-      {
-        id: "pts-3",
-        name: "PTS Invision Prototype",
-        url: "https://accentureoperations.invisionapp.com/console/share/T2RUBZ36WKH/985479606",
-      },
-      {
-        id: "pts-4",
-        name: "PTS Sketch",
-        url: "https://www.sketch.com/s/778a5b40-0a48-40b5-90e1-f974af88f19a",
-      },
-      {
-        id: "sde-4",
-        name: "GCP console",
-        url: "https://console.cloud.google.com/welcome?project=tfo-eu-dev-test-scp",
-      },
-    ],
+  id: "pts-2",
+      name: "Developer Studio",
+      url: "https://dev.azure.com/TfO-DevOps-CCT-US/SynOps_31124/_sprints/taskboard/Prompt%20Studio%20Buildouts/SynOps_31124/Prompt%20Studio/DS_Sprint_3",
   },
   {
-    category: "SynOps for Delivery Execution",
-    links: [
-      {
-        id: "sde-1",
-        name: "SDE-React-Repo",
-        url: "https://dev.azure.com/TfO-DevOps-CCT-US/SynOps%20for%20Delivery%20Execution_173668_349703/_git/sde-dev-react-p0",
-      },
-      {
-        id: "sde-2",
-        name: "SDE-DevSecOps-board",
-        url: "https://dev.azure.com/TfO-DevOps-CCT-US/SynOps%20for%20Delivery%20Execution_173668_349703/_boards/board/t/DevSecOps%20Findings%20Board/Stories",
-      },
-      {
-        id: "sde-3",
-        name: "SDE-Pipelines",
-        url: "https://dev.azure.com/TfO-DevOps-CCT-US/SynOps%20for%20Delivery%20Execution_173668_349703/_build",
-      },
-      {
-        id: "sde-4",
-        name: "GCP console",
-        url: "https://console.cloud.google.com/welcome?project=tfo-us-dev-dgp",
-      },
-    ],
+  id: "pts-3",
+      name: "Figma - DTP Assist",
+      url: "https://www.figma.com/file/7wc1kRYGo0V2ANWCB2C2gG/DTP-Co-Pilot-Assist?type=design&node-id=4483-63747&mode=design&t=jIvo7ln0PIKIUKSi-0",
   },
   {
-    category: "General",
-    links: [
-      {
-        id: "general - 1",
-        name: "Qwik labs",
-        url: "https://www.cloudskillsboost.google/course_templates/50",
-      },
-      {
-        id: "general - 2",
-        name: "Teams Automate flow",
-        url: "https://www.damobird365.com/teams-status-update-via-power-automate/",
-      },
-      {
-        id: "general - 3",
-        name: "SI Primer",
-        url: "https://mylearning.accenture.com/mylearningui/learner/coursedetail/2054826",
-      },
-      {
-        id: "general-4",
-        name: "Details",
-        url: "https://ast.accenture.com/Atlas/MyTeam/wfrmEmployeeOfficialInfo.aspx?strModule=Office%20Details",
-      },
-    ],
+  id: "pts-4",
+      name: "Figma - Developer Studio",
+      url: "https://www.figma.com/file/6zbvlz0RtmbUFnWY0j6rKh/Prompt-Tuning-Studio?type=design&node-id=188-5042&mode=design&t=VoXuWq2hXsFI4TJl-0",
+  },
+    {
+      id: "pts-5",
+      name: "PTS React Repo",
+      url: "https://dev.azure.com/TfO-DevOps-CCT-US/SynOps_31124/_git/SynOps.MSTeamAppEnablementUI/branches",
+    },
+    {
+      id: "pts-6",
+      name: "PTS Pipelines",
+      url: "https://dev.azure.com/TfO-DevOps-CCT-US/SynOps_31124/_build",
+    },
+    {
+      id: "pts-7",
+      name: "PTS Invision Prototype",
+      url: "https://accentureoperations.invisionapp.com/console/share/T2RUBZ36WKH/985479606",
+    },
+    {
+      id: "pts-4",
+      name: "PTS Sketch",
+      url: "https://www.sketch.com/s/778a5b40-0a48-40b5-90e1-f974af88f19a",
+    },
+    {
+      id: "pts-5",
+      name: "GCP console",
+      url: "https://console.cloud.google.com/welcome?project=tfo-eu-dev-test-scp",
+    },
+  ],
+},
+{
+  category: "Risk control platform",
+  links: [
+    {
+      id: "rcp-1",
+      name: "Scrum team 1 sprint board",
+      url: "https://dev.azure.com/TfO-DevOps-CCT-US/Risk%20Control%20Platform_244725/_sprints/backlog/Scrum-Team1/Risk%20Control%20Platform_244725/",
+    },
+    {
+      id: "rcp-2",
+      name: "React repos",
+      url: "https://dev.azure.com/TfO-DevOps-CCT-US/Risk%20Control%20Platform_244725/_git/ControlMonitoringFramework_UI/branches",
+    },
+    {
+      id: "rcp-3",
+      name: "API repos",
+      url: "https://dev.azure.com/TfO-DevOps-CCT-US/Risk%20Control%20Platform_244725/_git/CMF_API_LandscapeControl/branches",
+    },
+    {
+      id: "rcp-4",
+      name: "Pipelines",
+      url: "https://dev.azure.com/TfO-DevOps-CCT-US/Risk%20Control%20Platform_244725/_build",
+    },
+    {
+      id: "rcp-5",
+      name: "Invision Prototype",
+      url: "https://accentureoperations.invisionapp.com/console/share/MD7SG8T5V23/985480080/play",
+    },
+    {
+      id: "rcp-6",
+      name: "Sketch",
+      url: "https://www.sketch.com/s/7986ae17-5651-4304-aea1-95ac34f6136b/a/GowZO77",
+    },
+    {
+      id: "rcp-7",
+      name: "Data model excel",
+      url: "https://ts.accenture.com/:x:/s/RiskFrameworkPlatform/EUyI9hoJUWxCh1hSHdAVqdUBtKJlhAQmwvrwrtoBMqUmow?email=gunadevan.desing%40accenture.com&e=4%3AXGzyVg&at=9",
+    },
+    {
+      id: "rcp-8",
+      name: "Leave tracker",
+      url: "https://ts.accenture.com/:x:/r/sites/RiskFrameworkPlatform/_layouts/15/Doc.aspx?sourcedoc=%7BDED5F598-003D-4865-83BB-150630FC2077%7D&file=RCP-LeaveTracker-FY23.xlsx&action=default&mobileredirect=true&cid=538b9adf-cbc8-47d4-8e03-809cb3781e01",
+    },
+    {
+      id: "rcp-9",
+      name: "GCP console",
+      url: "https://console.cloud.google.com/welcome?project=tfo-usn-4959-dev-riskframework",
+    },
+  ],
+},
+{
+  category: "SynOps for Delivery Execution",
+  links: [
+    {
+      id: "sde-1",
+      name: "SDE-React-Repo",
+      url: "https://dev.azure.com/TfO-DevOps-CCT-US/SynOps%20for%20Delivery%20Execution_173668_349703/_git/sde-dev-react-p0",
+    },
+    {
+      id: "sde-2",
+      name: "SDE-DevSecOps-board",
+      url: "https://dev.azure.com/TfO-DevOps-CCT-US/SynOps%20for%20Delivery%20Execution_173668_349703/_boards/board/t/DevSecOps%20Findings%20Board/Stories",
+    },
+    {
+      id: "sde-3",
+      name: "SDE-Pipelines",
+      url: "https://dev.azure.com/TfO-DevOps-CCT-US/SynOps%20for%20Delivery%20Execution_173668_349703/_build",
+    },
+    {
+      id: "sde-4",
+      name: "GCP console",
+      url: "https://console.cloud.google.com/welcome?project=tfo-us-dev-dgp",
+    },
+  ],
+},
+{
+  category: "General",
+  links: [
+  {
+  id: "general-5",
+  name:"Credly",
+  url:"https://www.credly.com/earner/dashboard"
   },
   {
-    category: "Accenture links",
-    links: [
-      {
-        id: "accenture-1",
-        name: "My Equipments",
-        url: "https://support.accenture.com/support_portal/?id=acn_my_devices",
-      },
-      {
-        id: "accenture-2",
-        name: "Portal",
-        url: "https://portal.accenture.com/",
-      },
-      {
-        id: "accenture-3",
-        name: "My Competency",
-        url: "https://mycompetency.accenture.com/#/home",
-      },
-      {
-        id: "accenture-4",
-        name: "System enablement",
-        url: "https://ts.accenture.com/sites/India_info/Survey/SitePages/Home.aspx?CT=1623913577327&OR",
-      },
-      {
-        id: "accenture-5",
-        name: "My Sign-ins",
-        url: "https://mysignins.microsoft.com/security-info",
-      },
-      {
-        id: "accenture-6",
-        name: "AOC",
-        url: "https://aoc.accenture.com/Sunguard_London/httpstest.html#",
-      },
-      {
-        id: "accenture-7",
-        name: "BPS portal",
-        url: "https://bps-portal.accenture.com/portal/pages/Accenture/home",
-      },
-      {
-        id: "accenture-8",
-        name: "Performance & rewards",
-        url: "https://rewards.accenture.com/statement/2082514",
-      },
-      {
-        id: "accenture-9",
-        name: "Accenture appreciation gift cards",
-        url: "https://accentureappreciationgiftcards.maestro-rewards.com/app/",
-      },
-    ],
+  id: "general-6",
+  name:"Linun foundation",
+  url:"https://trainingportal.linuxfoundation.org/learn/course/green-software-for-practitioners-lfc131/introduction/welcome-to-lfc131?page=5"
   },
   {
-    category: "TFO Learning boards",
-    links: [
-      {
-        id: "tfo-1",
-        name: "TFO Learning board - secure development training framework core",
-        url: "https://connectedlearning.accenture.com/learningboard/1183831-tfo-secure-development-training-framework-core?check_logged_in=1",
-      },
-      {
-        id: "tfo-2",
-        name: "TFO Learning board - sql injection vulnerabilities",
-        url: "https://connectedlearning.accenture.com/learningboard/1180455-tfo-secure-development-avoiding-sql-injection-vulnerabilities",
-      },
-      {
-        id: "tfo-3",
-        name: "TFO Learning board - Session handling",
-        url: "https://mediaexchange.accenture.com/media/t/1_n7ybopld",
-      },
-    ],
-  },
-  {
-    category: "Github co-pilot",
-    links: [
-      {
-        id: "github-1",
-        name: "Github co-pilot",
-        url: "https://ts.accenture.com/:w:/s/InnovationRapidDev/Ed60MElM4iZOtsbL6uViWw4BdT3aIf7IHxyunc-7cAQWuA?e=7L0awG",
-      },
-    ],
-  },
-  {
-    category: "MS SQL Learning",
-    links: [
-      {
-        id: "sql-1",
-        name: "Database Fundamentals",
-        url: "https://learn.microsoft.com/en-us/shows/dbfundamentals/",
-      },
-      {
-        id: "sql-2",
-        name: "SQL tutorials - W3Schools",
-        url: "https://www.w3schools.com/sql/",
-      },
-      {
-        id: "sql-3",
-        name: "SQL tutorials - Tutorialspoint",
-        url: "https://www.tutorialspoint.com/ms_sql_server/index.htm",
-      },
-      {
-        id: "sql-4",
-        name: "Transact SQL reference",
-        url: "https://learn.microsoft.com/en-us/sql/t-sql/language-reference?view=sql-server-ver15",
-      },
-      {
-        id: "sql-5",
-        name: "Database engine tutorials",
-        url: "https://learn.microsoft.com/en-us/sql/relational-databases/database-engine-tutorials?view=sql-server-ver15",
-      },
-      {
-        id: "sql-6",
-        name: "Quackit",
-        url: "https://www.quackit.com/sql_server/sql_server_2016/tutorial/",
-      },
-      {
-        id: "sql-7",
-        name: "Educational SQL resources",
-        url: "https://learn.microsoft.com/en-us/sql/sql-server/educational-sql-resources?view=sql-server-ver15",
-      },
-      {
-        id: "sql-8",
-        name: "Query tuning techniques",
-        url: "https://www.youtube.com/watch?v=FoTMJFZ4wwg",
-      },
-      {
-        id: "sql-9",
-        name: "SQL Server Performance Tuning",
-        url: "https://www.youtube.com/watch?v=uDFX1YHfRqo",
-      },
-      {
-        id: "sql-10",
-        name: "SQL server technical documentation",
-        url: "https://learn.microsoft.com/en-us/sql/sql-server/?view=sql-server-ver15",
-      },
-    ],
-  },
+  id: "general-7",
+  name:"Future Talent Platform",
+  url:"https://futuretalentplatform.com/login?return=%2Fuser%2Fhome"
+  },	  
+    {
+      id: "general - 1",
+      name: "Qwik labs",
+      url: "https://www.cloudskillsboost.google/course_templates/50",
+    },
+    {
+      id: "general - 2",
+      name: "Teams Automate flow",
+      url: "https://www.damobird365.com/teams-status-update-via-power-automate/",
+    },
+    {
+      id: "general - 3",
+      name: "SI Primer",
+      url: "https://mylearning.accenture.com/mylearningui/learner/coursedetail/2054826",
+    },
+    {
+      id: "general-4",
+      name: "Details",
+      url: "https://ast.accenture.com/Atlas/MyTeam/wfrmEmployeeOfficialInfo.aspx?strModule=Office%20Details",
+    },
+  ],
+},
+{
+  category: "Accenture links",
+  links: [
+    {
+      id: "accenture-1",
+      name: "My Equipments",
+      url: "https://support.accenture.com/support_portal/?id=acn_my_devices",
+    },
+    {
+      id: "accenture-2",
+      name: "Portal",
+      url: "https://portal.accenture.com/",
+    },
+    {
+      id: "accenture-3",
+      name: "My Competency",
+      url: "https://mycompetency.accenture.com/#/home",
+    },
+    {
+      id: "accenture-4",
+      name: "System enablement",
+      url: "https://ts.accenture.com/sites/India_info/Survey/SitePages/Home.aspx?CT=1623913577327&OR",
+    },
+    {
+      id: "accenture-5",
+      name: "My Sign-ins",
+      url: "https://mysignins.microsoft.com/security-info",
+    },
+    {
+      id: "accenture-6",
+      name: "AOC",
+      url: "https://aoc.accenture.com/Sunguard_London/httpstest.html#",
+    },
+    {
+      id: "accenture-7",
+      name: "BPS portal",
+      url: "https://bps-portal.accenture.com/portal/pages/Accenture/home",
+    },
+    {
+      id: "accenture-8",
+      name: "Performance & rewards",
+      url: "https://rewards.accenture.com/statement/2082514",
+    },
+    {
+      id: "accenture-9",
+      name: "Accenture appreciation gift cards",
+      url: "https://accentureappreciationgiftcards.maestro-rewards.com/app/",
+    },
+  ],
+},
+{
+  category: "TFO Learning boards",
+  links: [
+    {
+      id: "tfo-1",
+      name: "TFO Learning board - secure development training framework core",
+      url: "https://connectedlearning.accenture.com/learningboard/1183831-tfo-secure-development-training-framework-core?check_logged_in=1",
+    },
+    {
+      id: "tfo-2",
+      name: "TFO Learning board - sql injection vulnerabilities",
+      url: "https://connectedlearning.accenture.com/learningboard/1180455-tfo-secure-development-avoiding-sql-injection-vulnerabilities",
+    },
+    {
+      id: "tfo-3",
+      name: "TFO Learning board - Session handling",
+      url: "https://mediaexchange.accenture.com/media/t/1_n7ybopld",
+    },
+  ],
+},
+{
+  category: "Github co-pilot",
+  links: [
+    {
+      id: "github-1",
+      name: "Github co-pilot",
+      url: "https://ts.accenture.com/:w:/s/InnovationRapidDev/Ed60MElM4iZOtsbL6uViWw4BdT3aIf7IHxyunc-7cAQWuA?e=7L0awG",
+    },
+  ],
+},
+{
+  category: "MS SQL Learning",
+  links: [
+    {
+      id: "sql-1",
+      name: "Database Fundamentals",
+      url: "https://learn.microsoft.com/en-us/shows/dbfundamentals/",
+    },
+    {
+      id: "sql-2",
+      name: "SQL tutorials - W3Schools",
+      url: "https://www.w3schools.com/sql/",
+    },
+    {
+      id: "sql-3",
+      name: "SQL tutorials - Tutorialspoint",
+      url: "https://www.tutorialspoint.com/ms_sql_server/index.htm",
+    },
+    {
+      id: "sql-4",
+      name: "Transact SQL reference",
+      url: "https://learn.microsoft.com/en-us/sql/t-sql/language-reference?view=sql-server-ver15",
+    },
+    {
+      id: "sql-5",
+      name: "Database engine tutorials",
+      url: "https://learn.microsoft.com/en-us/sql/relational-databases/database-engine-tutorials?view=sql-server-ver15",
+    },
+    {
+      id: "sql-6",
+      name: "Quackit",
+      url: "https://www.quackit.com/sql_server/sql_server_2016/tutorial/",
+    },
+    {
+      id: "sql-7",
+      name: "Educational SQL resources",
+      url: "https://learn.microsoft.com/en-us/sql/sql-server/educational-sql-resources?view=sql-server-ver15",
+    },
+    {
+      id: "sql-8",
+      name: "Query tuning techniques",
+      url: "https://www.youtube.com/watch?v=FoTMJFZ4wwg",
+    },
+    {
+      id: "sql-9",
+      name: "SQL Server Performance Tuning",
+      url: "https://www.youtube.com/watch?v=uDFX1YHfRqo",
+    },
+    {
+      id: "sql-10",
+      name: "SQL server technical documentation",
+      url: "https://learn.microsoft.com/en-us/sql/sql-server/?view=sql-server-ver15",
+    },
+  ],
+},
 ];
